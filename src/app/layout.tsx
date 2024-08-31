5d1ddccc73e0245cb85e78f2fef69475a65f7611
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2024-08-31 06:51:42 +0530

    add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import { ThemeProvider } from "@/context/ThemeContext";
 import { DarkModeToggle } from "@/components/DarkModeToggle";
 import FooterWithSnakeGame from "@/components/footer/FooterWithGame";
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -30,6 +31,7 @@ export default function RootLayout({
             <FooterWithSnakeGame />
           </div>
           <SpeedInsights />
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
