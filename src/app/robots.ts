c1fbe4bbe909a6a3b4df6cf6d9ccf9491eb6a5cc
Sourav Kumar Nanda <souravkumarnanda4@gmail.com>
2026-02-06 22:09:16 +0530

    fix: allow /api/og in robots.txt for Twitter OG image fetching

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,11 +4,13 @@ export default function robots(): MetadataRoute.Robots {
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://www.souravinsights.com'
   
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/private/', '/api/'],
-    },
+    rules: [
+      {
+        userAgent: '*',
+        allow: ['/api/og'],
+        disallow: ['/private/', '/api/'],
+      },
+    ],
     sitemap: `${baseUrl}/sitemap.xml`,
   }
 }
